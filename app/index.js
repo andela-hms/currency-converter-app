3ce22176f9278e2a534e77a32de3ea0a98da3024
andela-hms <humphrey.musonye@andela.com>
2018-04-10 09:41:41 +0300

    feat(home): create homescreen

--- app/index.js
+++ app/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import EstyleSheet from 'react-native-extended-stylesheet';
+import Home from './screens/Home';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-export default () =>
-  (<View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-    <Text>Changes you make will automatically reload.</Text>
-    <Text>Shake your phone to open the developer menu.</Text>
-  </View>);
+EstyleSheet.build({
+  $primaryBlue: "#4F6D7A",
+})
+export default () => <Home />;
