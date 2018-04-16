02a152223a3be4adcdb591cfce32efc8c009cfee
andela-hms <humphrey.musonye@andela.com>
2018-04-16 23:28:20 +0300

    feat(button): add button structure

---
+++ app/components/Button/styles.js
@@ -0,0 +1,7 @@
+import { Dimensions } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import { StyleSheet } from 'react-native';
+
+export default EStyleSheet.create({
+  
+});
