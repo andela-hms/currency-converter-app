fe5d179a22bac4d9602d1bf9708a5028b91de68e
andela-hms <humphrey.musonye@andela.com>
2018-04-20 22:17:03 +0300

    feat(home-screen): create logo animation

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,23 +1,67 @@
-import React from 'react';
-import { View, Image, ImageBackground, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Image, ImageBackground, Text, Keyboard, Animated } from 'react-native';
 
 import styles from './styles';
 
-const Logo = () => (
-  <View style={styles.container}>
-    <ImageBackground
-      resizeMode='contain'
-      style={ styles.containerImage }
-      source={require('./images/background.png')}
-    >
-      <Image
-        resizeMode='contain'
-        style={styles.logo} 
-        source={require('./images/logo.png')}
-      />
-    </ImageBackground>
-    <Text style={ styles.text }>Currency Converter</Text>
-  </View>
-);
+const ANIMATION_DURATION = 250;
+
+class Logo extends Component {
+  constructor (props) {
+    super(props);
+
+    this.containerImageWidth = new Animated.Value(styles.$largeContainerSize);
+    this.imageWidth = new Animated.Value(styles.$largeImageSize);
+  }
+  componentDidMount() {
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardWillShow', this.keyboardDidShow);
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardWillHide', this.keyboardDidHide);
+  }
+
+  componentWillUnmount() {
+    this.keyboardDidShowListener.remove();
+    this.keyboardDidHideListener.remove();
+  }
+
+  keyboardDidShow = () => {
+    console.log('keyboard did show');
+    Animated.timing(this.containerImageWidth, {
+      toValue: styles.$smallContainerSize,
+      duration: ANIMATION_DURATION
+    }).start();
+  };
+
+  keyboardDidHide = () => {
+    console.log('keyboard did hide');
+  };
+
+  render () {
+    const containerImageStyle = [
+      styles.containerImage,
+      { width: this.containerImageWidth, height: this.containerImageWidth},
+    ];
+
+    const imageStyle = [
+      styles.logo,
+      { width: this.imageWidth },
+    ];
+
+    return (
+      <Animated.View style={styles.container}>
+        <ImageBackground
+          resizeMode='contain'
+          style={containerImageStyle}
+          source={require('./images/background.png')}
+        >
+          <Image
+            resizeMode='contain'
+            style={imageStyle}
+            source={require('./images/logo.png')}
+          />
+        </ImageBackground>
+        <Text style={ styles.text }>Currency Converter</Text>
+      </Animated.View>
+    );
+  }
+}
 
 export default Logo;
--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,6 +4,12 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 const imageWidth = Dimensions.get('window').width / 2;
 
 export default EStyleSheet.create({
+
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
+
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -13,12 +19,12 @@ export default EStyleSheet.create({
   containerImage: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth,
+    width: '$largeContainerSize',
+    height: '$largeContainerSize',
   },
 
   logo: {
-    width: imageWidth / 2,
+    width: '$largeImageSize',
   },
 
   text: {
